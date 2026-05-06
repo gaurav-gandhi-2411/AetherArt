30ceeccbad96ffb42cdfaf9c645bb25b2cdbf98f
Gaurav Gandhi <gaurav.gandhi2411@gmail.com>
2026-05-06 19:38:16 +0530

    feat: add aetherart/visualization package (ChartCanvas, palette)
    Copy of collision-aware chart renderer from shelfsense-m5 with:
    - docstring updated to reference AetherArt
    - matplotlib.use("Agg") removed (AetherArt uses default backend)
    - __init__.py re-exports ChartCanvas and all palette constants

---
+++ aetherart/visualization/__init__.py
@@ -0,0 +1,25 @@
+from aetherart.visualization.charts import (
+    BLUE,
+    GREEN,
+    GREY,
+    LGREY,
+    ORANGE,
+    PURPLE,
+    RED,
+    TEAL,
+    TEAL_LIGHT,
+    ChartCanvas,
+)
+
+__all__ = [
+    "ChartCanvas",
+    "BLUE",
+    "ORANGE",
+    "GREEN",
+    "RED",
+    "PURPLE",
+    "TEAL",
+    "TEAL_LIGHT",
+    "GREY",
+    "LGREY",
+]
